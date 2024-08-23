9675e9d79484025b637f7eabf37f632b05768702
Muhammad Saad <130956259+Saadimehar@users.noreply.github.com>
2024-08-23 12:23:48 +0000

    complete

--- Project1/jscode.js
+++ Project1/jscode.js
@@ -1,22 +1,55 @@
+console.log("Muhammad Saad");
 const button = document.querySelectorAll('.button');
-const body = document.querySelector('.body');
+const body = document.querySelector('body');
 
 button.forEach(function(button){
     console.log(button);
     button.addEventListener('click', function (e) {
         console.log(e);
         console.log(e.target);
-        if (e.target.id = 'grey') {
+        if (e.target.id === 'grey') {
            body.style.backgroundColor = e.target.id;
+           body.style.color='black';
         }
-        if (e.target.id = 'white') {
+        if (e.target.id === 'white') {
             body.style.backgroundColor = e.target.id;
+            body.style.color='black';
          }
-         if (e.target.id = 'blue') {
+         if (e.target.id === 'blue') {
             body.style.backgroundColor = e.target.id;
+            body.style.color='black';
          }
-         if (e.target.id = 'yellow') {
+         if (e.target.id === 'yellow') {
             body.style.backgroundColor = e.target.id;
+            body.style.color='black';
+         }
+         if (e.target.id === 'green') {
+            body.style.backgroundColor = e.target.id;
+            body.style.color='black';
+         }
+         if (e.target.id === 'black') {
+            body.style.backgroundColor = e.target.id;   
+            body.style.color='white';
+         }
+         if (e.target.id === 'red') {
+            body.style.backgroundColor = e.target.id;
+            body.style.color='black';
+         }
+         if (e.target.id === 'pink') {
+            body.style.backgroundColor = e.target.id;
+            body.style.color='black';
+         }
+         if (e.target.id === 'orange') {
+            body.style.backgroundColor = e.target.id;
+            body.style.color='black';
+         }
+         if (e.target.id === 'purple') {
+            body.style.backgroundColor = e.target.id;
+            body.style.color='black';
+         }
+         if (e.target.id === 'brown') {
+            body.style.backgroundColor = e.target.id;
+            body.style.color='black';
          }
     });
 });
