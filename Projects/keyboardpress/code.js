306579e77c24970b3a697a5f8e67f4846417a9e9
Muhammad Saad <130956259+Saadimehar@users.noreply.github.com>
2024-08-28 10:46:25 +0000

    keyboard

--- Projects/keyboardpress/code.js
+++ Projects/keyboardpress/code.js
@@ -1,22 +1,18 @@
-const insert = document.getElementById('insert');
+const insert = document.getElementById('insert')
 
-window.addEventListener('keydown', (e) => {
-  insert.innerHTML = `
-    <div class='color'>
-    <table>
-    <tr>
-      <th>Key</th>
-      <th>Keycode</th> 
-      <th>Code</th>
-    </tr>
-    <tr>
-      <td>${e.key === ' ' ? 'Space' : e.key}</td>
-      <td>${e.keyCode}</td> 
-      <td>${e.code}</td>
-    </tr>
-    
-  </table>
-    </div>
-  `;
-});
-
+window.addEventListener('keydown',(e)=>{
+  insert.innerHTML=`
+  <div class='color'>
+  <table>
+  <tr>
+    <th>key</th>
+    <th>keyCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key === ' ' ? "Space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>`
+})
